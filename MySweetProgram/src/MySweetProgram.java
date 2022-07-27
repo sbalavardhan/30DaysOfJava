c3b8b021d655b8ee80d6f32c5772b2d36dfd227f
Sripuram <847546@corpaa.aa.com>
2022-07-27 06:28:17 -0500

    first commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your Name");
+		
+		Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		
+
+		int i = 0;
+		
+		do{
+		System.out.println("i is: " + i);
+		++i;
+		} while(i < 10);
+		
+		
+        in.close();
+	}
+
+}
