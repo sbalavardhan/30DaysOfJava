5f0cd22be198c4bbe3d8a9c9652d4a0aa0727f78
Sripuram <847546@corpaa.aa.com>
2022-07-31 14:27:01 -0500

    Convert queue to stack

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,23 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
-
-	public static void main(String[] args) throws FileNotFoundException {
-				
-		Scanner in = new Scanner(new File("students.txt"));
+	
+	public static void main(String[] args) {
 		
-		List<String> students = new ArrayList<String>();
-		while (in.hasNextLine()) {
-			students.add(in.nextLine());
-		}
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("Name: " + students.get(i));
-		}
-	   in.close();
-		
-	}
+	LinkedList<String> names = new LinkedList<String>();			
+	 names.push("Caleb");
+	 names.push("Rebecca");
+	 names.push("John");
+	 System.out.println(names.pop());
+	 System.out.println(names.pop());
+	 System.out.println(names.pop());
+ }
 }
