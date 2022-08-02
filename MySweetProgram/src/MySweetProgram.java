99a13ace84e780ddf2b87eb2f40476acc9fec195
Sripuram <847546@corpaa.aa.com>
2022-08-02 00:58:07 -0500

    iterators

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -2,8 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MySweetProgram {
@@ -14,8 +16,15 @@ public class MySweetProgram {
 	 names.push("Caleb");
 	 names.push("Rebecca");
 	 names.push("John");
-	 System.out.println(names.pop());
-	 System.out.println(names.pop());
-	 System.out.println(names.pop());
+	 
+	ListIterator<String> it = names.listIterator();
+	it.next();
+	it.next();
+	it.add("Susan");	
+	
+	for(String s:names) {
+		System.out.println(s);
+	}
+	
  }
 }
