665144444de7d3ab8164c4f44a4c837c0a9ab05b
Sripuram <847546@corpaa.aa.com>
2022-08-03 15:46:05 -0500

    working with 2d arrays

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -11,20 +11,21 @@ import java.util.Scanner;
 public class MySweetProgram {
 	
 	public static void main(String[] args) {
+	
+		int [][] data = {
+				{4, 6, 3, 10},
+				{4, 2, 40, 1, 5, 3, 2, 30},
+				{5, 34, 1, 43},
 		
-	LinkedList<String> names = new LinkedList<String>();			
-	 names.push("Caleb");
-	 names.push("Rebecca");
-	 names.push("John");
-	 
-	ListIterator<String> it = names.listIterator();
-	it.next();
-	it.next();
-	it.add("Susan");	
-	
-	for(String s:names) {
-		System.out.println(s);
-	}
-	
- }
+
+		};
+         
+					
+		for (int i = 0; i < data.length;  i++) {
+			for (int j = 0; j < data[i].length;  j++) {
+			 System.out.print(data[i][j] + "  ");
+			}
+		System.out.println();					
+		}
+  }
 }
